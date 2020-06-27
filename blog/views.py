56c77f932ee1e5d97b606bafdd7a1ad793344c05
Princeshaw <prince54shaw@gmail.com>
2020-06-28 01:56:45 +0530

    Adding blogs and css for home page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import blog
 
 def allblogs(request):
     blogs = blog.objects
     return render(request,"blog/allblogs.html",{'blogs':blogs})
+def readblog(request,blog_id):
+    detail = get_object_or_404(blog,pk=blog_id)
+    return render(request,'blog/readblog.html',{'detail':detail})    
